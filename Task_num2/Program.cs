50c394f362e3b9718c4723aee9df38dcf010621f
Kelt <kelt_dmitriy@gmail.com>
2023-04-24 17:53:27 +0300

    add My Homework 001

---
+++ Task_num2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Семинар 1.
+//Домашнее задание.
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//Выполнил: Кельт Дмитрий Владимирович (kelt_1981@mail.ru)
+//Решение:
+
+System.Console.WriteLine("Введите первое число ");
+string a = Console.ReadLine();
+int i = int.Parse(a);
+
+System.Console.WriteLine("Введите второе число ");
+string b = Console.ReadLine();
+int j = int.Parse(b);
+
+if (i>j)
+{
+    System.Console.WriteLine($"Большее число {i}");
+    System.Console.WriteLine($"Меньшее число {j}");
+}
+else if (j>i)
+{
+    System.Console.WriteLine($"Большее число {j}");
+    System.Console.WriteLine($"Меньшее число {i}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
---
+++ Task_num8/Program.cs
@@ -0,0 +1,22 @@
+﻿//Семинар 1.
+//Домашнее задание.
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//Выполнил: Кельт Дмитрий Владимирович (kelt_1981@mail.ru)
+//Решение:
+
+System.Console.WriteLine("Введите число в интервале от 1 до N:");
+string text = Console.ReadLine();
+int a = int.Parse(text);
+int count = 1;
+
+System.Console.WriteLine($"Четные числа от 1 до {a}: ");
+
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.Write($"{count} ");
+        count = count + 1;
+    }
+    count = count +1;
+}
